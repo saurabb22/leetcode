6838be5fe222e94b1c26295005df4a49339ecb9c
Saurabh Kumar <120946428+Saurabh4325@users.noreply.github.com>
2025-09-10 20:53:05 +0530

    Time: 3 ms (90.66%), Space: 44.4 MB (26.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        // nums2 ke elements ka index map me daal do
-        for (int i = 0; i < nums2.length; i++) {
-            map.put(nums2[i], i);
-        }
-
-        int arr[] = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-            int indexInNums2 = map.get(nums1[i]);  // nums1[i] ka index nums2 me
-            arr[i] = -1;  // default agar koi next greater na mile
-
-            // ab check karo nums2 me aage koi greater hai kya
-            for (int j = indexInNums2 + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    arr[i] = nums2[j];
-                    break;  // pehle hi greater mil gaya to stop
-                }
+        int arr[] = new int[nums2.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i=nums2.length-1; i>=0; i--){
+            while(!st.isEmpty() && nums2[i]>=st.peek()){
+                st.pop();
             }
+            if(st.isEmpty()){
+                arr[i] = -1;
+            }else{
+                arr[i] = st.peek();
+            }
+            st.push(nums2[i]);
+        }
+        // creating hashmap
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums2.length; i++){
+            map.put(nums2[i],arr[i]);
         }
 
-        return arr;
+        // creating ans arr
+        int ans[] = new int[nums1.length];
+        for(int i=0; i<nums1.length; i++){
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
     }
 }
